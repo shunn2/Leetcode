af5f0858e4e6655a29fea9f2025a2dd4ac4c74c2
Seonghoon Cho <86822777+shunn2@users.noreply.github.com>
2023-10-02 18:19:27 +0900

    Time: 67 ms (85.00%), Space: 45.2 MB (71.67%) - LeetHub

---
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} colors
+ * @return {boolean}
+ */
+var winnerOfGame = function(colors) {
+    let alice = 0;
+    let bob = 0;
+    let temp = 1;
+    for(let i = 1; i<colors.length; i++){
+        let prev = colors[i-1];
+        let cur = colors[i];
+        if(prev === cur){
+            temp += 1;
+        }else{
+            if(temp >= 3){
+                if(prev === 'A'){
+                    alice += temp - 2;
+                }else{
+                    bob += temp - 2;
+                }
+            }
+            temp = 1;
+        }
+        
+        if(i === colors.length - 1){
+            if(temp >= 3){
+                if(cur === 'A'){
+                    alice += temp - 2;
+                }else{
+                    bob += temp - 2;
+                }
+            }
+        }
+    }
+    console.log('alice', alice);
+    console.log('bob', bob);
+    return alice > bob;
+};
