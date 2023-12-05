c3bc4fd748ce70f6b996daf93539b367ef2bb596
Seonghoon Cho <86822777+shunn2@users.noreply.github.com>
2023-12-05 10:21:13 +0900

    Time: 56 ms (24.92%), Space: 41.8 MB (50.45%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numberOfMatches = function(n) {
+    const isOdd = (n) => {
+        return n % 2 === 0 ? false : true;
+    }
+    let result = 0;
+    while(n > 1){
+        let temp = Math.floor(n / 2);
+        result += temp;
+        if(isOdd(n)){
+            n = temp + 1;
+        }else{
+            n = temp;
+        }
+    }
+    
+    return result;
+};
