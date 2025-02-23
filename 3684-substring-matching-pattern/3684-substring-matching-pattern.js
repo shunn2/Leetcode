b734cb97a94f17f8b407017b082c754ba3c7bc7c
Seonghoon Cho <86822777+shunn2@users.noreply.github.com>
2025-02-24 01:46:29 +0900

    Time: 13 ms (13.85%), Space: 56.2 MB (5.19%) - LeetHub

---
+++ 3684-substring-matching-pattern/3684-substring-matching-pattern.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var hasMatch = function(s, p) {
+    const parts = p.split('*');
+    const prefix = parts[0];
+    const suffix = parts[1];
+
+    console.log('suffix',suffix)
+
+    let startIndex = s.indexOf(prefix);
+    let endIndex = s.lastIndexOf(suffix);
+
+    if(startIndex === -1 || endIndex === -1){
+        return false;
+    }
+    
+
+    if(startIndex + prefix.length <= endIndex){
+        return true;
+    }
+
+    return false;
+};
