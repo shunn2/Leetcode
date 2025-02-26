67b267c955e9fb25a3aef3be456a43e3fd6ddfcb
Seonghoon Cho <86822777+shunn2@users.noreply.github.com>
2025-02-26 21:05:37 +0900

    Time: 41 ms (63.06%), Space: 60.5 MB (10.45%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+    let result = 0;
+    nums.sort((a, b) => a - b);
+
+    for(let i = 0; i < nums.length; i+=2){
+        result += nums[i];
+    }
+
+    return result;
+};
