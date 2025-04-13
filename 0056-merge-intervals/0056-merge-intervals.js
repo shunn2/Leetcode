517200958cae297f832a2487a2b60a2900cf661d
Seonghoon Cho <86822777+shunn2@users.noreply.github.com>
2025-04-14 02:22:27 +0900

    Time: 17 ms (17.42%), Space: 65.1 MB (46.93%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    intervals.sort((a,b)=>a[0]-b[0]);
+    const result = [intervals[0]];
+
+    let index = 1;
+
+    for(; index < intervals.length;){
+        const [a, b] = result[result.length - 1];
+        const [c, d] = intervals[index];
+
+        if(b >= c){
+            const min = a;
+            const max = d;
+            if(b > d){
+                result[result.length - 1] = [a, b];
+            }else{
+                result[result.length - 1] = [a, d];
+            }
+            
+        }else{
+            result.push(intervals[index]);
+        }
+
+        index++;
+    }
+
+    return result;
+};
