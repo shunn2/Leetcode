993d5cb5e146941a9888a48a514e9b0f61afe93c
Seonghoon Cho <86822777+shunn2@users.noreply.github.com>
2025-08-11 22:30:38 +0900

    Time: 18 ms (56.52%), Space: 64.7 MB (100%) - LeetHub

---
+++ 0881-loud-and-rich/0881-loud-and-rich.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} richer
+ * @param {number[]} quiet
+ * @return {number[]}
+ */
+var loudAndRich = function(richer, quiet) {
+    const n = quiet.length;
+    const g = Array.from({length: n}, ()=>[]);
+    const answer = Array(n).fill(-1);
+    for(const [a, b] of richer){
+        g[b].push(a);
+    }
+
+    function dfs(p){
+        if(answer[p] !== -1){
+            return answer[p];
+        }
+        answer[p] = p;
+        for(const richer of g[p]){
+            const man = dfs(richer);
+            if(quiet[man] < quiet[answer[p]]){
+                answer[p] = man;
+            }
+        }
+
+        return answer[p];
+    }
+
+    for(let i = 0; i < n; i++){
+        dfs(i);
+    }
+
+    return answer;
+};
