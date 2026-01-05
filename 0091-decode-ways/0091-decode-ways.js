f57f0c5b8ea7ee349cbd9701fcc632777cd67d33
Seonghoon Cho <86822777+shunn2@users.noreply.github.com>
2026-01-05 22:20:28 +0900

    Time: 0 ms (100%), Space: 53.6 MB (95.72%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+    const dp = new Array(s.length +1).fill(0);
+    dp[s.length] = 1;
+
+    for(let i = s.length -1; i >= 0 ; i--){
+        if(s[i] == 0) continue;
+        dp[i]  = dp[i+1];
+        if(s[i] + s[i+1] <= 26) dp[i] += dp[i+2];
+    }
+
+    return dp[0];
+};
