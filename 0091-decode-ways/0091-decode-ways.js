f6ccdc9e6b4256df6063caab44ef381fe4d79a15
Seonghoon Cho <86822777+shunn2@users.noreply.github.com>
2026-01-05 22:27:02 +0900

    Time: 0 ms (100%), Space: 54.2 MB (86.33%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.js
+++ 0091-decode-ways/0091-decode-ways.js
@@ -3,14 +3,18 @@
  * @return {number}
  */
 var numDecodings = function(s) {
-    const dp = new Array(s.length +1).fill(0);
-    dp[s.length] = 1;
+    const n = s.length;
+    const dp = new Array(n + 1).fill(0);
 
-    for(let i = s.length -1; i >= 0 ; i--){
-        if(s[i] == 0) continue;
-        dp[i]  = dp[i+1];
-        if(s[i] + s[i+1] <= 26) dp[i] += dp[i+2];
+    dp[0] = 1;
+    dp[1] = s[0] !== '0' ? 1 : 0;
+
+    for (let i = 2; i <= n; i++) {
+        if (s[i - 1] !== '0') dp[i] += dp[i - 1];
+
+        const two = Number(s[i - 2] + s[i - 1]);
+        if (two >= 10 && two <= 26) dp[i] += dp[i - 2];
     }
 
-    return dp[0];
+    return dp[n];
 };
